8b493cf3af2db5fe34f617b0702c8791570cbbd2
Miguel <mamezola@unomaha.edu>
2022-01-28 16:56:11 +0000

    hello

---
+++ hello/hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Miguel Amezola
+ *         Ryan Gallo
+ * Date: 2022/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Miguel Amezola!\n");
+
+  return 0;
+}
